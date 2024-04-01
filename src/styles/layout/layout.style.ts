6e167d345ef9ee91df9ec7786bbfac7810114e55
heejung0413 <acb4287@gmail.com>
2024-04-01 23:11:57 +0900

    Design: 사이드바 퍼블리싱, 디자인

--- src/styles/layout/layout.style.ts
+++ src/styles/layout/layout.style.ts
@@ -31,18 +31,50 @@ export const LOGO = styled.a`
 `;
 
 export const ProfileContainer = styled.div`
-  width: 200px;
-  margin: 50px auto;
+  width: auto;
+  margin: 50px 29px;
   background-color: #f8f8f8;
 `;
 
-export const ImageContainer = styled.img`
-  width: 80%;
+export const ImageContainer = styled.div`
+  margin: 0 auto;
+  width: 200px;
 `;
 
 export const ProfileDetailContainer = styled.div`
-  width: 150px;
-  height: 200px;
-  margin: 10px auto;
-  background-color: #fffce9;
+  margin: 10px 20px;
+  padding: 5px 10px;
+`;
+
+export const ProfileName = styled.p`
+  font-size: 25px;
+  color: #4d5053;
+  margin: 0;
+`;
+
+export const DesriptionName = styled.p`
+  font-size: 15px;
+  color: #4d5053;
+  margin: 10px 0;
+`;
+
+export const IconStyle = styled.div`
+  background-color: #f4f0ec;
+  border-radius: 45%;
+  width: auto;
+  height: 30px;
+  display: inline-block;
+  margin: auto 0;
+  margin-right: 5px;
+
+  .icon {
+    font-size: 20px;
+    margin: 0 3px;
+    padding: 5px;
+  }
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
