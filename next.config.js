115497b6e1299eec43f4b97590935addbdc84cc9
heejung0413 <acb4287@gmail.com>
2024-04-03 23:35:36 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-    optimizeImages: true,
-    optimizeFonts: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
