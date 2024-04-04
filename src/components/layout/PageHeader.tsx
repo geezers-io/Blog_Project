590a147dfae8ac56368ea26d69a5a27830a20e1e
Ellsy23 <dlwnsgus3153@naver.com>
2024-04-04 13:51:48 +0900

    feat: Adjust header item position

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -16,58 +16,48 @@ const PageHeader = () => {
 
   return (
     <S.BGHeaderContainer>
-      <Grid container justifyContent="center">
+      <Grid container>
         <Grid item xs={12}>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="center"
-            style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px' }}
-          >
-            <Grid item>
-              <Image src="/logo.png" alt="logo" width={100} height={50} />
-            </Grid>
-            <Grid item>
-              <MusicPlayer />
-            </Grid>
-          </Grid>
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid item>
-              <Button style={{ color: 'black' }}>
-                <Link href="/">
-                  <a style={{ textDecoration: 'none', color: 'black' }}>홈</a>
-                </Link>
-              </Button>
-              <Button style={{ color: 'black' }}>
-                <Link href="/about">
-                  <a style={{ textDecoration: 'none', color: 'black' }}>내 소개</a>
-                </Link>
-              </Button>
-              <Button style={{ color: 'black' }}>
-                <Link href="/guestbook">
-                  <a style={{ textDecoration: 'none', color: 'black' }}>방명록</a>
-                </Link>
-              </Button>
-              <Button onClick={() => setSearchVisible(!searchVisible)}>
-                <Search style={{ color: 'black' }} />
-              </Button>
-              <Button onClick={colorMode.toggleColorMode} style={{ color: 'black' }}>
-                {theme.palette.mode === 'dark' ? <DarkMode /> : <LightMode />}
-              </Button>
-            </Grid>
-          </Grid>
-          {searchVisible && (
-            <Grid container justifyContent="center">
+          <Grid container style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px' }}>
+            <Grid container>
               <Grid item>
-                <TextField
-                  id="outlined-basic"
-                  label="검색"
-                  variant="outlined"
-                  style={{ marginTop: '10px', width: '200px' }}
-                />
+                <Image src="/logo.png" alt="logo" width={100} height={50} />
+              </Grid>
+
+              <Grid item marginLeft="36%">
+                <Grid container justifyContent="center">
+                  <Button style={{ color: 'black' }}>
+                    <Link href="/">
+                      <a style={{ textDecoration: 'none', color: 'black' }}>홈</a>
+                    </Link>
+                  </Button>
+                  <Button style={{ color: 'black' }}>
+                    <Link href="/about">
+                      <a style={{ textDecoration: 'none', color: 'black' }}>내 소개</a>
+                    </Link>
+                  </Button>
+                  <Button style={{ color: 'black' }}>
+                    <Link href="/guestbook">
+                      <a style={{ textDecoration: 'none', color: 'black' }}>방명록</a>
+                    </Link>
+                  </Button>
+                  <Button onClick={() => setSearchVisible(!searchVisible)}>
+                    <Search style={{ color: 'black' }} />
+                  </Button>
+                </Grid>
+              </Grid>
+              {searchVisible && <TextField id="outlined-basic" label="검색" variant="outlined" />}
+            </Grid>
+
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <MusicPlayer />
               </Grid>
             </Grid>
-          )}
+            <Button onClick={colorMode.toggleColorMode} style={{ color: 'black' }}>
+              {theme.palette.mode === 'dark' ? <DarkMode /> : <LightMode />}
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </S.BGHeaderContainer>
