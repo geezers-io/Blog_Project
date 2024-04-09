d049546b9091562058342bd642cbd086ba55b590
Ellsy23 <dlwnsgus3153@naver.com>
2024-04-09 14:04:33 +0900

    feat: Apply filter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { Container, Typography, Button, Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
+import {
+  Container,
+  Typography,
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Menu,
+  MenuItem,
+} from '@mui/material';
 
 const IndexPage: React.FC = () => {
   const [blogPosts, setBlogPosts] = useState([
@@ -18,6 +28,16 @@ const IndexPage: React.FC = () => {
     },
   ]);
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleFilterButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleFilterClose = () => {
+    setAnchorEl(null);
+  };
+
   const addNewPost = () => {
     const newPost = {
       id: blogPosts.length + 1,
@@ -32,6 +52,23 @@ const IndexPage: React.FC = () => {
     <main>
       <div style={{ padding: '8rem 0 6rem' }}>
         <Container maxWidth="md">
+          <div style={{ marginBottom: '1rem' }}>
+            <Button variant="outlined" color="primary" onClick={handleFilterButtonClick}>
+              Filter
+            </Button>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleFilterClose}>
+              <MenuItem onClick={handleFilterClose}>최신순</MenuItem>
+              <MenuItem onClick={handleFilterClose}>댓글 많은 순</MenuItem>
+              <MenuItem onClick={handleFilterClose}>오래된 순</MenuItem>
+              <MenuItem onClick={handleFilterClose}>인기많은 순</MenuItem>
+            </Menu>
+
+            <Button variant="contained">
+              <Link href="/guestbook" passHref>
+                <a style={{ textDecoration: 'none', color: 'black' }}>글쓰기</a>
+              </Link>
+            </Button>
+          </div>
           {blogPosts.map(post => (
             <Card key={post.id} style={{ marginBottom: '2rem' }}>
               <Link href={`/blogs/article${post.id}`}>
