875329083af6f036d5253a07518c703e9bcc5e05
Ellsy23 <dlwnsgus3153@naver.com>
2024-04-09 14:05:03 +0900

    feat: Login publishing

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,7 +1,7 @@
+import Image from 'next/image';
 import { useState } from 'react';
-import { TextField, Button, Grid, Typography, Link } from '@mui/material';
-import * as C from '@/styles/common/common.style';
-import * as S from '@/styles/sign/signIn.style';
+import { LockOutlined as LockOutlinedIcon } from '@mui/icons-material';
+import { TextField, Button, Grid, Typography, Link, Avatar, Box } from '@mui/material';
 
 const SignInPage = () => {
   const [username, setUsername] = useState('');
@@ -12,52 +12,77 @@ const SignInPage = () => {
     console.log('Password:', password);
   };
 
-  return (
-    <S.BGContainer>
-      <C.CommonText size="20px">로그인</C.CommonText>
+  const kakaoLogo = '/kakao.png';
+  const naverLogo = '/naver.png';
 
-      <S.SignUpContainer>
-        <Grid container spacing={2} direction="column">
-          <Grid item>
-            <Typography variant="body1">아이디</Typography>
+  return (
+    <Grid container justifyContent="center" height="100vh">
+      <Grid item>
+        <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8 }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
             <TextField
+              margin="normal"
+              required
+              fullWidth
               id="username"
               label="아이디"
-              variant="outlined"
+              name="username"
+              autoFocus
               value={username}
               onChange={e => setUsername(e.target.value)}
             />
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">비밀번호</Typography>
             <TextField
-              id="password"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
               label="비밀번호"
-              variant="outlined"
               type="password"
+              id="password"
+              autoComplete="current-password"
               value={password}
               onChange={e => setPassword(e.target.value)}
             />
-          </Grid>
+            <Button fullWidth variant="contained" color="primary" sx={{ mt: 3, mb: 2 }} onClick={handleSignIn}>
+              로그인
+            </Button>
+            <Grid container spacing={2}>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  비밀번호를 잊으셨나요?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  회원가입
+                </Link>
+              </Grid>
+            </Grid>
+            <Typography variant="body1" sx={{ mt: 3 }}>
+              소셜 로그인
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Button fullWidth variant="outlined">
+                  <Image src={kakaoLogo} width={100} height={40} />
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button fullWidth variant="outlined">
+                  <Image src={naverLogo} width={100} height={40} />
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
         </Grid>
-
-        <Button variant="contained" onClick={handleSignIn}>
-          로그인
-        </Button>
-      </S.SignUpContainer>
-
-      <Link href="/forgot-password" color="inherit">
-        아이디/비밀번호 찾기
-      </Link>
-      <Link href="/signup" color="inherit">
-        회원가입
-      </Link>
-
-      <Typography variant="body1">소셜 로그인</Typography>
-      <Button variant="outlined">카카오 로그인</Button>
-      <Button variant="outlined">네이버 로그인</Button>
-    </S.BGContainer>
+      </Grid>
+    </Grid>
   );
 };
 
