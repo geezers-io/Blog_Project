17f9aea576b5122f0bc6c3c9b63d6cfa79cc667e
LimHeeJung <acb4287@gmail.com>
2024-05-16 23:22:39 +0900

    temp

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -22,7 +22,7 @@ const PageHeader = () => {
                 <Grid container justifyContent="flex-end">
                   <Button style={{ color: 'black', marginRight: '10px' }}>
                     <Link href="/about">
-                      <a style={{ textDecoration: 'none', color: 'black' }}>프로필 정보</a>
+                      <a style={{ textDecoration: 'none', color: 'black' }}>프로필 정보 dd</a>
                     </Link>
                   </Button>
                   <Button style={{ color: 'black', marginRight: '10px' }}>
